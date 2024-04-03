e052ae4931647f37cbe63f865e41d8cda3792aa1
Rodrigo Ariel Ledezma Lopez <rodri.ledezma9483@gmail.com>
2024-04-03 13:52:26 -0400

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>Bienvenido a Gestion Academica</h1>
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Esto es login</h1>
+</template>
+
+<style>
+
+</style>
